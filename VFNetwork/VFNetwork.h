04aab0cd14d237d76dd38fbfc9a5055e99065831
Victor Freitas <victor.freitas@stone.com.br>
2020-03-13 14:18:05 -0300

    Initial Commit

---
+++ VFNetwork/VFNetwork.h
@@ -0,0 +1,19 @@
+//
+//  VFNetwork.h
+//  VFNetwork
+//
+//  Created by Victor Freitas on 13/03/20.
+//  Copyright © 2020 Victor Freitas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VFNetwork.
+FOUNDATION_EXPORT double VFNetworkVersionNumber;
+
+//! Project version string for VFNetwork.
+FOUNDATION_EXPORT const unsigned char VFNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VFNetwork/PublicHeader.h>
+
+
