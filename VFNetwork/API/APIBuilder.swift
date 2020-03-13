e44999189865886af060ff9b048f190d6548ae52
Victor Freitas <victor.freitas@stone.com.br>
2020-03-13 15:21:21 -0300

    First Commit

---
+++ VFNetwork/API/APIBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  APIBuilder.swift
+//  inWave Back Office
+//
+//  Created by Victor Alves De Freitas on 26/06/19.
+//  Copyright © 2019 brvlab. All rights reserved.
+//
+
+import Foundation
+
+public protocol APIBuilder {
+    var baseURL: URL { get }
+    var path: URLPath { get }
+    var httpMethod: HTTPMethods { get }
+    var headers: HTTPHeader { get }
+    var task: HTTPTask { get }
+    var cacheable: Bool { get }
+}
+
